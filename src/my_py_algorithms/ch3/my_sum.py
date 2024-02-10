7bc2d17c8580231d559933d7068c060714d672b3
t_ishikawa <tissi0708@gmail.com>
2024-02-10 19:52:21 +0900

    update my_sum function

--- src/my_py_algorithms/ch3/my_sum.py
+++ src/my_py_algorithms/ch3/my_sum.py
@@ -1,9 +1,8 @@
-def my_sum(numbers):
-    count = len(numbers)
-    if count == 1:
-        return numbers[0]
+def my_sum(numbers, index=0):
+    if index == len(numbers) - 1:
+        return numbers[index]
     else:
-        return numbers[0] + my_sum(numbers[1:])
+        return numbers[index] + my_sum(numbers, index + 1)
 
 
 print(my_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
