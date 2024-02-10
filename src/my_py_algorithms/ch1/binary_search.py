a0e7136e48e2f6ad92db29f955cbefc17834b50b
t_ishikawa <tissi0708@gmail.com>
2024-02-10 15:29:11 +0900

    add pytest

--- src/my_py_algorithms/ch1/binary_search.py
+++ src/my_py_algorithms/ch1/binary_search.py
@@ -1,4 +1,3 @@
-
 def binary_search(arr, item):
     low = 0
     high = len(arr) - 1
@@ -14,8 +13,9 @@ def binary_search(arr, item):
             low = mid + 1
     return None
 
+
 my_list = [1, 3, 8, 23, 34, 55, 76, 92]
 
-# print(binary_search(my_list, 55))
-# print(binary_search(my_list, 30))
+print(binary_search(my_list, 55))
+print(binary_search(my_list, 30))
 print(binary_search(my_list, 3))
---
+++ tests/ch1/test_binary_search.py
@@ -0,0 +1,20 @@
+import pytest
+from my_py_algorithms.ch1.binary_search import binary_search
+
+
+def test_binary_search_found():
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert binary_search(arr, 4) == 3
+    assert binary_search(arr, 1) == 0
+    assert binary_search(arr, 10) == 9
+
+
+def test_binary_search_not_found():
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert binary_search(arr, -1) is None
+    assert binary_search(arr, 11) is None
+    assert binary_search(arr, 0) is None
+
+
+def test_binary_search_empty_array():
+    assert binary_search([], 1) is None
